c13ec38e2581b617201a46f91bf78822dbdd83f6
Rogerio Felix <rogerio.felix.ext@espm.br>
2024-11-12 11:50:12 -0300

    refactor: enhance layout component with improved metadata and viewport settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,26 @@
-import Favicon from '../../public/favicon.ico'
+import type { Viewport } from 'next'
+import type { Metadata } from 'next'
+
 import '@/styles/globals.css'
+import Favicon from '../../public/favicon.ico'
 import { IntlClientProvider } from '@/translations/intlClientContext'
 import { ScrollToTop } from '@/utilities/ScrollToTop'
 import { ChecklistsProvider } from '@/utilities/checklistsContext'
-import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: {
     default: 'Design System Checklist',
     template: 'Design System Checklist',
   },
-  description: 'Test your design system against the best practices',
+  description:
+    'An open-source checklist to help you plan, build and grow your design system.',
   icons: [{ rel: 'icon', url: Favicon.src }],
+  robots: 'index, follow',
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
 }
 
 type RootLayoutProps = {
@@ -21,12 +30,43 @@ type RootLayoutProps = {
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang={'en'}>
+      <head>
+        <meta name="googlebot" content="index,follow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://designsystemchecklist.com/" />
+        <meta property="og:title" content="Design System Checklist" />
+        <meta
+          property="og:description"
+          content="An open-source checklist to help you plan, build and grow your design system."
+        />
+        <meta
+          property="og:image"
+          content="https://designsystemchecklist.com/meta-v2.png"
+        />
+        <meta property="og:image:alt" content="Design System Checklist" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://designsystemchecklist.com/"
+        />
+        <meta property="twitter:title" content="Design System Checklist" />
+        <meta
+          property="twitter:description"
+          content="An open-source checklist to help you plan, build and grow your design system."
+        />
+        <meta
+          property="twitter:image"
+          content="https://designsystemchecklist.com/meta-v2.png"
+        />
+      </head>
       <body>
-        <ScrollToTop />
-
         <IntlClientProvider>
           <ChecklistsProvider>
-            <div id="__DESYSCHECK">{children}</div>
+            <div id="__DESYSCHECK">
+              {children}
+              <ScrollToTop />
+            </div>
           </ChecklistsProvider>
         </IntlClientProvider>
       </body>
